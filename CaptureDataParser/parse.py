422435269a9e2caacff7624287ebc0a3b15dde87
SCHWMAX <max.schwenzer@voith.com>
2024-04-22 15:16:40 +0200

    if recording is broken

--- CaptureDataParser/parse.py
+++ CaptureDataParser/parse.py
@@ -7,6 +7,7 @@ from CaptureDataParser.parse_payload import parse_payload
 from CaptureDataParser.CapturePayload import CapturePayload
 
 from typing import Union, List
+import warnings
 
 
 def read_json(file: Union[Path, str]) -> dict:
@@ -45,19 +46,29 @@ def parse(
         content[fl.name] = read_capture_recording(fl)
 
     # find start file: loop through footers
-    next_filename = None
+    start_filename = None
     for filename, (_, _, footer) in content.items():
-        if footer["FilePathChain"]["Previous"] is None:
-            next_filename = filename
-            break
+        previous_filename = footer["FilePathChain"]["Previous"]
+        actual_filename = footer["FilePathChain"]["Actual"]
+        assert filename == actual_filename
 
-    if next_filename is None:
-        raise FileNotFoundError("No start file of recording found.")
+        if previous_filename is None:
+            start_filename = actual_filename
+            break
+        elif previous_filename not in content:
+            warnings.warn(f"File {previous_filename} not found. Recording broken. Using a later start.")
+            start_filename = actual_filename
+        else:
+            start_filename = previous_filename
+
+    if start_filename is None:
+        raise FileNotFoundError(f"No start file of recording found in {[el.name for el in files]}.")
 
     # walk through contents
     signals = dict()
     head0 = None
     i = 1
+    next_filename = start_filename
     while True:
         header, payload, footer = content[next_filename]
 
@@ -79,6 +90,9 @@ def parse(
         # update next filename
         next_filename = footer["FilePathChain"]["Next"]
         if next_filename is None:
+            break
+        elif next_filename not in content:
+            warnings.warn(f"File {next_filename} not found. Recording broken. Terminating recording earlier.")
             break
         i += 1
 
