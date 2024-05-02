05261b9c5d8c93480067ed1a855217f9c9873677
SCHWMAX <max.schwenzer@voith.com>
2024-05-02 17:01:16 +0200

    limit signal length

--- extract_signal_from_recordings.py
+++ extract_signal_from_recordings.py
@@ -9,18 +9,24 @@ from utils import get_files, save_dict_of_dataframes, default_argument_parser, p
 from typing import Union
 
 
+def get_time_period(time: pd.Series, in_seconds: bool = False) -> int | float:
+    dt_ns = int(np.nanmedian(time[:10000].diff()))  # in nanoseconds
+    return (dt_ns / 10 ** 9) if in_seconds else dt_ns
+
+
 def aggregate_signal(
         signals: pd.DataFrame,
         method: str,
         window_size: Union[int, float],
-        in_seconds: bool = False
+        in_seconds: bool = False,
+        time: pd.Series = None,
 ) -> Union[pd.DataFrame, None]:
     method = method.lower()
 
     # aggregate signal
-    dft = signals.drop("Time", axis=1)
     if in_seconds:
-        dt = np.nanmedian(signals["Time"].diff())  # nanosecond
+        assert len(signals) == len(time), "Length of signals and time series do not match"
+        dt = get_time_period(time)  # nanosecond
         if dt < 1:
             warnings.warn(f"Sample time is less than 1 nanosecond. Implausible. Skipping file {fl.name}.")
             return None
@@ -39,15 +45,15 @@ def aggregate_signal(
 
     # methods
     if (len(method) > 3) and (method[:3] == "abs"):
-        dft = dft.abs()
+        signals = signals.abs()
         method = method[3:]
 
     if method in ("rms", "rmse"):
-        df_aggregated = dft.pow(2).rolling(**kwargs).apply(lambda x: np.sqrt(x.mean()))
+        df_aggregated = signals.pow(2).rolling(**kwargs).apply(lambda x: np.sqrt(x.mean()))
     elif method == "sum":
-        df_aggregated = dft.rolling(**kwargs).mean()
+        df_aggregated = signals.rolling(**kwargs).mean()
     elif method == "mean":
-        df_aggregated = dft.rolling(**kwargs).mean()
+        df_aggregated = signals.rolling(**kwargs).mean()
     else:
         raise ValueError(f"Unknown method: {method}")
 
@@ -60,11 +66,15 @@ if __name__ == "__main__":
     # add arguments to aggregate signals
     parser.add_argument("--window-size", type=float, default=-1,
                         help="Window size. See --in-seconds to make it a duration")
+    parser.add_argument("--limit", type=float, default=-1,
+                        help="Limit the signal length. May be an index or a duration if --in-seconds is used.")
     parser.add_argument("--in-seconds", action="store_true",
-                        help="Window size. See --in-seconds to make it a duration")
+                        help="Window size and limit will be interpreted as a time value.")
     parser.add_argument("--method", type=str, default="rms",
                         help="Aggregation method. Can be 'rms', 'sum', 'mean', 'absSum', or 'absMean'.")
+
     opt = parse_arguments(parser)
+
 
     # process user inputs
     keys = opt.signal
@@ -81,12 +91,14 @@ if __name__ == "__main__":
         for fl, df in get_files(opt.source, opt.file_extension):
             try:
                 sig = df[key]
+                time = df["Time"] if opt.in_seconds or opt.window_size else None
             except KeyError as ex:
                 raise Exception(f"Signal '{key}' not found in {fl.as_posix()}. "
                                 f"Available signals are {', '.join(df.keys())}")
             except Exception as ex:
                 raise Exception(f"Failed to process {fl.as_posix()} with exception: {ex}")
 
+            # aggregate signal
             if opt.window_size > 0:
                 sig = aggregate_signal(
                     sig,
@@ -94,11 +106,22 @@ if __name__ == "__main__":
                     window_size=opt.window_size,
                     in_seconds=opt.in_seconds
                 )
+            # limit signal length
+            if opt.limit > 0:
+                fct = (1 / get_time_period(time, True)) if opt.in_seconds else 1
+                end = int(round(opt.limit * fct))
+                # slice signal
+                sig = sig[:end]
+
             data[fl.stem] = sig
 
         # save data
         if len(data) > 0:
-            filename = export_path / f"{key.replace('|', '_')}_{len(data)}.json"
-            save_dict_of_dataframes(filename, data)
+            filename = f"{key.replace('|', '_')}_{len(data)}"
+            if opt.window_size > 0:
+                filename += f"_{opt.method}{opt.window_size}" + "s" if opt.in_seconds else ""
+            if opt.limit > 0:
+                filename += f"_{opt.limit}" + "s" if opt.in_seconds else ""
+            save_dict_of_dataframes(export_path / filename, data)
         else:
             warnings.warn(f"No data found for key '{key}'")
