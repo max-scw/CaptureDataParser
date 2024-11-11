b0cb7dabd193dc32709c11052b9b21a173f1ed48
SCHWMAX <max.schwenzer@voith.com>
2024-11-11 09:28:43 +0100

    better import

--- CaptureDataParser/__init__.py
+++ CaptureDataParser/__init__.py
@@ -1,9 +1,14 @@
 # classes and data models
-from CaptureDataParser.CapturePayload import CapturePayload
-from CaptureDataParser.HeaderData import (HeaderData, SignalHeaderLF, SignalHeaderHF)
+from .CapturePayload import CapturePayload
+from .HeaderData import (HeaderData, SignalHeaderLF, SignalHeaderHF)
 
 # parsers
-from CaptureDataParser.parse import parse
-from CaptureDataParser.parse_header import parse_header
-from CaptureDataParser.parse_payload import parse_payload
+from .parse import parse
+from .parse_header import parse_header
+from .parse_payload import parse_payload
 
+# utils
+#from CaptureDataParser.utils import check_key_pattern
+
+# allow simpler import
+__all__ = ["CapturePayload", "HeaderData", "SignalHeaderLF", "parse", "parse_header", "parse_payload"]
